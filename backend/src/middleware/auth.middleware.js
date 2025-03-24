f5502edc1a039c2c011779de87ab33a3681ad125
Neilx21 <neil-kapadia.kapadia@city.ac.uk>
2025-03-24 22:39:51 +0000

    made frontend and backend structure with React, Vite, and Express; add Tailwind CSS and MongoDB connection

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.models.js";
+
+export const protectedRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt; //uses cookie-parser
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No Token Provided" });
+    }
+
+    //gives a decoded payload if valid, or return error/false if not valid
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res.status(
+        401,
+        json({ message: "Unauthorized - No Token Provided" })
+      );
+    }
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log("Error in protectedRoute middleware: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
---
+++ frontend/src/components/AuthImagePattern.jsx
@@ -0,0 +1,22 @@
+const AuthImagePattern = ({ title, subtitle }) => {
+  return (
+    <div className="lg:flex items-center justify-center bg-base-200 p-12">
+      <div className="text-center">
+        <div className="grid grid-cols-3 gap-3 mb-8">
+          {[...Array(9)].map((_, i) => (
+            <div
+              key={i}
+              className={`w-16 h-16 rounded-2xl bg-blue-200 ${
+                i % 2 === 0 ? "animate-pulse" : ""
+              }`}
+            />
+          ))}
+        </div>
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className="text-base-content/60">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthImagePattern;
