05c018f60cd4391dd36ddf8c4f920d1bdb338e8e
Neilx21 <neilkapadia.nk@gmail.com>
2025-04-15 13:31:49 +0100

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,15 @@ import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 //Middlwaare to parse JSON request body
 app.use(express.json());
@@ -27,6 +31,13 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
